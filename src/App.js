17523624f9ef6c1402e2fb2f62cdc19b1524e7a1
HannahConnolly <connollyhannah93@gmail.com>
2021-09-24 18:46:54 -0700

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+const endpoint = 'http://192.168.1.94:5555';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='has-background-light'>
+      <div className='columns'>
+        <div className='column' />
+        <div className='column' style={{ alignItems: 'center' }}>
+          <div style={{ marginTop: '200px' }} />
+          <h1 className='title has-text-centered'>Disable adlbocker</h1>
+          <div className='buttons'>
+            <button
+              className='button fa-align-center'
+              onClick={() => {
+                callBackendAPI(30, 'seconds');
+              }}
+            >
+              30 seconds
+            </button>
+            <button
+              className='button fa-align-center'
+              onClick={() => {
+                callBackendAPI(60, 'seconds');
+              }}
+            >
+              1 minute
+            </button>
+            <button
+              className='button fa-align-center'
+              onClick={() => {
+                callBackendAPI(300, 'seconds');
+              }}
+            >
+              5 minutes
+            </button>
+            <button
+              className='button fa-align-center'
+              onClick={() => {
+                callBackendAPI(60, 'minutes');
+              }}
+            >
+              1 hour
+            </button>
+          </div>
+        </div>
+        <div className='column' />
+      </div>
     </div>
   );
 }
+
+// function callDisabler(amount, type) {
+//   console.log(endpoint);
+//   callBackendAPI(amount, type);
+// }
+
+// fetching the GET route from the Express server which matches the GET route from server.js
+const callBackendAPI = async (amount, type) => {
+  console.log(`${endpoint}/?${type}=${amount}`);
+  await fetch(`${endpoint}/?${type}=${amount}`, {
+    headers: new Headers({}),
+  });
+};
 
 export default App;
